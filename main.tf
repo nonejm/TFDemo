57960d5cfc302102ce19d340e1439730be103b96
nonejm <41159689+nonejm@users.noreply.github.com>
2024-04-19 21:05:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
-test
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+resource "google_storage_bucket" "static" {
+  name          = "${random_id.bucket_prefix.hex}-new-bucket"
+  location      = "US"
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
+}
