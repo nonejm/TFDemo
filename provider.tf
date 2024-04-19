5e6f82a622968d0c672420f4371b50f1c53f4252
nonejm <41159689+nonejm@users.noreply.github.com>
2024-04-19 21:34:42 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,5 +2,5 @@ provider "google" {
   region      = "us-central1"
   zone        = "us-central1-c"
   project = "sylvan-cirrus-398113"
-  credentials = var.gcp-creds
+  credentials = file("./sylvan-cirrus-398113-8af11cfb5ef8.json")
 }
