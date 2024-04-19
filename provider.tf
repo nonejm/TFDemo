d97945471c3c6e05768ab0d5952bbae67127cc00
nonejm <41159689+nonejm@users.noreply.github.com>
2024-04-19 21:28:54 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,2 +1,5 @@
-project = "sylvan-cirrus-398113"
-email = "terraform-admin@sylvan-cirrus-398113.iam.gserviceaccount.com"
+provider "google" {
+  region      = "us-central1"
+  zone        = "us-central1-c"
+  project = "sylvan-cirrus-398113"
+}
