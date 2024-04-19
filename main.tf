a95401ca8f9a7854f889253e7b2384349e601c6c
nonejm <41159689+nonejm@users.noreply.github.com>
2024-04-19 21:26:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "google_storage_bucket" "static" {
   name          = "${random_id.bucket_prefix.hex}-new-bucket"
   location      = "US"
   storage_class = "STANDARD"
+  project = "sylvan-cirrus-398113"
 
   uniform_bucket_level_access = true
 }
