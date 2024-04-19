9cb72de6d91bb394f665ae9d81189267c13c163a
nonejm <41159689+nonejm@users.noreply.github.com>
2024-04-19 21:25:50 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,4 @@
 provider "google" {
-  project     = "sylvan-cirrus-398113"
-  email       = "terraform-admin@sylvan-cirrus-398113.iam.gserviceaccount.com"
   region      = "us-central1"
   zone        = "us-central1-c"
 }
