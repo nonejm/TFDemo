a414ba12275b058cf189536037d999c56afc5d5d
nonejm <41159689+nonejm@users.noreply.github.com>
2024-04-19 21:57:31 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,5 +2,4 @@ provider "google" {
   region      = "us-central1"
   zone        = "us-central1-c"
   project = "sylvan-cirrus-398113"
-  credentials = file("./sylvan-cirrus-398113-8af11cfb5ef8.json")
 }
